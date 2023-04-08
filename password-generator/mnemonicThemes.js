3b9b7e9efaffdfcf7b35920feb412b1e8b55ba3e
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2023-04-07 21:25:27 -0400

    Update mnemonicThemes.js
    updated animals lst

--- password-generator/mnemonicThemes.js
+++ password-generator/mnemonicThemes.js
@@ -30,10 +30,60 @@ export const mnemonicThemes = {
         C: "Cricket",
         // ...
     },
-    animals: {
-        A: "Antelope",
-        B: "Baboon",
-        C: "Cheetah",
-        // ...
-    },
+    
+animals: {
+    A: "ANTELOPE",
+    B: "BABOON",
+    C: "CHEETAH",
+    D: "DOLPHIN",
+    E: "ELEPHANT",
+    F: "FLAMINGO",
+    G: "GIRAFFE",
+    H: "HEDGEHOG",
+    I: "IGUANA",
+    J: "JAGUAR",
+    K: "KANGAROO",
+    L: "LEMUR",
+    M: "MONKEY",
+    N: "NUMBAT", // A small marsupial native to Australia
+    O: "OCTOPUS",
+    P: "PANDA",
+    Q: "QUOKKA", // A small marsupial native to Australia
+    R: "RHINOCEROS",
+    S: "SQUIRREL",
+    T: "TURTLE",
+    U: "URUTU", // A venomous snake from South America
+    V: "VULTURE",
+    W: "WALLABY",
+    X: "XENOPS", // A small bird from Central and South America
+    Y: "YAK",
+    Z: "ZEBRA",
+    a: "aquarium",
+    b: "burrow",
+    c: "carnivore",
+    d: "den",
+    e: "endangered",
+    f: "feathers",
+    g: "gills",
+    h: "habitat",
+    i: "incubation",
+    j: "jump",
+    k: "krill", // A small crustacean eaten by many marine animals
+    l: "lair",
+    m: "migration",
+    n: "nocturnal",
+    o: "omnivore",
+    p: "predator",
+    q: "quills", // Spines on animals like porcupines and hedgehogs
+    r: "reptile",
+    s: "swarm",
+    t: "territory",
+    u: "ungulate", // A group of large mammals that includes deer, cattle, and elephants
+    v: "venom",
+    w: "web",
+    x: "xeric", // An adaptation to living in dry environments
+    y: "young", // Referring to offspring of animals
+    z: "zoology",
+},
+
 };
