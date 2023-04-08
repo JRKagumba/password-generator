85c650e285f4a080de2264d5cdae38a9122f8f7c
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2023-04-07 20:53:05 -0400

    Create mnemonicThemes.js

---
+++ password-generator/mnemonicThemes.js
@@ -0,0 +1,39 @@
+// mnemonicThemes.js
+export const mnemonicThemes = {
+    military: {
+        A: "Alpha",
+        B: "Bravo",
+        C: "Charlie",
+        // ...
+    },
+    movies: {
+        A: "Avatar",
+        B: "Batman",
+        C: "Casablanca",
+        // ...
+    },
+    songs: {
+        A: "All-star",
+        B: "Bohemian Rhapsody",
+        C: "Crazy",
+        // ...
+    },
+    geography: {
+        A: "Argentina",
+        B: "Brazil",
+        C: "China",
+        // ...
+    },
+    sports: {
+        A: "Archery",
+        B: "Basketball",
+        C: "Cricket",
+        // ...
+    },
+    animals: {
+        A: "Antelope",
+        B: "Baboon",
+        C: "Cheetah",
+        // ...
+    },
+};
