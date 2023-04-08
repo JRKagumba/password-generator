5420a9719d83212e59cdd9fbe37aead18405949c
Joe Kagumba <63820705+JRKagumba@users.noreply.github.com>
2023-04-07 20:22:44 -0400

    updated JS script

--- password-generator/script.js
+++ password-generator/script.js
@@ -6,91 +6,37 @@ const lowerEl = document.getElementById("lower");
 const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 const generateEl = document.getElementById("generate");
+const darkModeEl = document.getElementById("dark-mode");
+const exportEl = document.getElementById("export");
 
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+=";
 
-function getLowercase() {
-    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
-}
-
-function getUppercase() {
-    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
-}
-
-function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function getSymbol() {
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
+const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 function generatePassword() {
     const len = lenEl.value;
+    const characters = [
+        ...(upperEl.checked ? upperLetters : ""),
+        ...(lowerEl.checked ? lowerLetters : ""),
+        ...(numberEl.checked ? numbers : ""),
+        ...(symbolEl.checked ? symbols : ""),
+    ];
 
-    let password = "";
-
-    if (upperEl.checked) {
-        password += getUppercase();
-    }
-
-    if (lowerEl.checked) {
-        password += getLowercase();
-    }
-
-    if (numberEl.checked) {
-        password += getNumber();
-    }
-
-    if (symbolEl.checked) {
-        password += getSymbol();
-    }
-
-    for (let i = password.length; i < len; i++) {
-        const x = generateX();
-        password += x;
-    }
-
-    pwEl.innerText = password;
+    return Array.from({ length: len }, () => getRandom(characters)).join("");
 }
 
-function generateX() {
-    const xs = [];
-    if (upperEl.checked) {
-        xs.push(getUppercase());
-    }
-
-    if (lowerEl.checked) {
-        xs.push(getLowercase());
-    }
-
-    if (numberEl.checked) {
-        xs.push(getNumber());
-    }
-
-    if (symbolEl.checked) {
-        xs.push(getSymbol());
-    }
-
-    if (xs.length === 0) return "";
-
-    return xs[Math.floor(Math.random() * xs.length)];
-}
-
-generateEl.addEventListener("click", generatePassword);
+generateEl.addEventListener("click", () => {
+    pwEl.innerText = generatePassword();
+});
 
 copyEl.addEventListener("click", () => {
+    if (!pwEl.innerText) return;
+
     const textarea = document.createElement("textarea");
-    const password = pwEl.innerText;
-
-    if (!password) {
-        return;
-    }
-
-    textarea.value = password;
+    textarea.value = pwEl.innerText;
     document.body.appendChild(textarea);
     textarea.select();
     document.execCommand("copy");
@@ -98,51 +44,8 @@ copyEl.addEventListener("click", () => {
     alert("Password copied to clipboard");
 });
 
-
-generateEl.addEventListener("click", () => {
-    let text = pwEl.innerText;
-    var array = text.split("");
-    var mnemonic = "";
-    
-    var dict = {
-        "a":"about","b":"board","c":"chest","d":"dwarf","e":"earth","f":"field","g":"globe",
-        "h":"heart","i":"image","j":"jokey","k":"khaki","l":"laser","m":"metal","n":"nacho",
-        "o":"oasis","p":"panel","q":"quail","r":"radio","s":"salad","t":"table","u":"ulcer",
-        "v":"vegan","w":"wharf","x":"xalam","y":"yacht","z":"zebra",
-
-        "A":"ANKARA","B":"BOGOTA","C":"CALLAO","D":"DOUALA","E":"ELPASO","F":"FOSHAN","G":"GDANSK",
-        "H":"HAVANA","I":"IBADAN","J":"JEDDAH","K":"KANPUR","L":"LONDON","M":"MUMBAI","N":"NAGPUR",
-        "O":"ODESSA","P":"PUEBLA","Q":"QUEENS","R":"RIYADH","S":"SYDNEY","T":"TAIPEI","U":"UJPEST",
-        "V":"VIENNA","W":"WARSAW","X":"XIAMEN","Y":"YANGON","Z":"ZURICH",
-
-    };
-
-    const keys = Object.keys(dict);
-
-
-
-    for (index = 0; index < array.length; index++) {
-        
-        if (index % 5 ==0) {
-            mnemonic += "\n"
-        }
-
-        if (keys.includes(array[index])) {
-            if (array[index] == array[index].toUpperCase()) {
-                mnemonic += dict[array[index]].fontcolor("#ff4500") + '  ';
-            }else {
-                mnemonic += dict[array[index]].fontcolor("#ffd700") + '  ';
-            }
-
-        }else if (!isNaN(array[index])){
-            mnemonic += array[index].fontcolor("#000000") + '  ';
-        }else {
-            mnemonic += array[index].fontcolor("#00bfff") + '  ';
-        }
-
-    }
-
-    // console.log(mnemonic);
-    document.getElementById("mnemonic").innerHTML = mnemonic; 
-
+darkModeEl.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
 });
+
+// Add your mnemonic generation and export logic here
